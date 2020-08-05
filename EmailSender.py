7f91e82b711db9d457fab2b7f5f0bb7d64b8fecf
am1089 <46304864+am1089@users.noreply.github.com>
2020-08-04 21:51:10 -0700

    Took out the get_contact function
    I took out the get_contact function and instead ran the main loop based on the row dictionaries made by the DictReader function.

--- EmailSender.py
+++ EmailSender.py
@@ -6,59 +6,41 @@ from email.mime.multipart import MIMEMultipart
 import csv
 from string import Template
 
-ID = 0
-FIRST = 1
-LAST = 2
-GENDER = 3
-EMAIL = 4
-IMAGE = 5
 Sender = 'Sender Name'
 SenderEmail = 'sender@gmail.com'
-SenderPassword = '*******'
-
-def get_contacts(filename, UserIds):
-    Users = {}
-    with open(filename, newline = '') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            Users[row['id']] = [row['first'], row['last'], row['gender'], row['email'], row['image']]
-            UserIds.append(row['id'])
-    return Users
+SenderPassword = '******'
 
 def read_template(filename):
     with open(filename, 'r', encoding='utf-8') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
-UserIds = []    
-Users = get_contacts('UserInfo.csv', UserIds)
+with open('UserInfo.csv', newline = '') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row['last']
+        imgfile = row['image']
+        fp = open(imgfile, 'rb')
+        image = MIMEImage(fp.read())
+        fp.close()
+        if row['gender'] == 'Male':
+            gender = 'Mr.'
+        else:
+            gender = 'Miss'
 
-for i in range(len(UserIds)):
-    id = UserIds[i]
-    Info = Users[id]
-    name = Info[LAST-1]
-    imgfile = Info[IMAGE-1]
-    fp = open(imgfile, 'rb')
-    image = MIMEImage(fp.read())
-    fp.close()
-    if Info[GENDER-1] == 'Male':
-        gender = 'Mr.'
-    else:
-        gender = 'Miss'
+        email = MIMEMultipart()
+        email['from'] = Sender
+        email['to'] = row['email']
+        email['subject'] = 'Hi'
 
-    email = MIMEMultipart()
-    email['from'] = Sender
-    email['to'] = Info[EMAIL-1]
-    email['subject'] = 'Hi'
+        message_template = read_template('MessageTemplate.txt')
+        message = MIMEText(message_template.substitute(NAME = name.title(), GENDER = gender))
+        email.attach(message)
+        email.attach(image)
 
-    message_template = read_template('MessageTemplate.txt')
-    message = MIMEText(message_template.substitute(NAME = name.title(), GENDER = gender))
-    email.attach(message)
-    email.attach(image)
-
-    with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls() # encryption method 
-        smtp.login(SenderEmail, SenderPassword)
-        smtp.send_message(email)
-        print('Yes')
+        with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls() # encryption method 
+            smtp.login(SenderEmail, SenderPassword)
+            smtp.send_message(email)
+            print('Email sent to ' + row['first'] + ' ' + row['last'])
